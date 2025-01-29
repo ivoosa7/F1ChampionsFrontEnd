3631c5ee284d15b0022f72b1970c03fd20b07795
Ivo Araujo <ivo.souza-23@outlook.com>
2025-01-28 23:59:44 -0300

    Rotas feitas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChampionsF1Component } from './champions-f1/champions-f1.component';
+import { AddChampionsComponent } from './add-champions/add-champions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'champions', component:ChampionsF1Component},
+  {path:'', redirectTo:'/champions', pathMatch:'full'},
+  {path:'addchampions', component:AddChampionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
